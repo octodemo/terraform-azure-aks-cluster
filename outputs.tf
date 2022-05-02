0d3e81b5c9d577cbbd5857f1bcd9ef12057af52d
Peter Murray <681306+peter-murray@users.noreply.github.com>
2022-05-02 10:22:21 +0000

    Updating outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "aks_cluster" {
   value = {
-    resource_group     = azurerm_resource_group.aks_cluster_resource_group.name
-    cluster_name       = azurerm_kubernetes_cluster.aks_cluster.name
-    egress_ip          = data.azurerm_public_ip.aks_cluster.ip_address
-    client_certificate = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate
-    kube_config        = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
+    resource_group  = azurerm_resource_group.aks_cluster_resource_group.name
+    cluster_name    = azurerm_kubernetes_cluster.aks_cluster.name
+    egress_ip       = data.azurerm_public_ip.aks_cluster.ip_address
+    kube_config     = azurerm_kubernetes_cluster.aks_cluster.kube_config.0
+    kube_config_raw = azurerm_kubernetes_cluster.aks_cluster.kube_config_raw
   }
   sensitive = true
 }
